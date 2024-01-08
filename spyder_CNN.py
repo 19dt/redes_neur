250321154e1e1f96279174377457f0b6878a5df8
Adrian Santos Zurita <adriansantoszurita@iMac-de-Adrian.local>
2024-01-08 19:19:57 +0100

    subida de archivos a repo

---
+++ spyder_CNN.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 25 13:12:28 2023
+
+@author: adriansantoszurita
+"""
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
